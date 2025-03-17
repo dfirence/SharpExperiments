96a7a621c568f6e7c8004c886945037be4fc4f02
dfirence <dfirence@users.noreply.github.com>
2025-03-16 20:56:23 -0400

    add cli arg

--- SharpExperiments/Program.cs
+++ SharpExperiments/Program.cs
@@ -2,26 +2,36 @@
 
 using System;
 using SharpExperiments.REPL;
+using SharpExperiments.ML.Classifiers;
 
 public class Program
 {
+    private static string s_helpView = $@"
+    carlos_diaz | @dfirence
+            
+        SharpExperiments CLI
+            
+        Usage:
+                --repl      Start the interactive REPL mode.
+                --mlc       Run ML Classifier Example";
+    
     public static void Main(string[] args)
     {
-        if (args.Length > 0 && args[0].Equals("--repl", StringComparison.OrdinalIgnoreCase))
+        if (args.Length < 0)
+        {
+            Console.WriteLine(s_helpView);
+            return
+        }
+        
+        if (args[0].Equals("--repl", StringComparison.OrdinalIgnoreCase))
         {
             // Start the interactive REPL mode
             REPLConsole.Start();
         }
-        else
+        
+        if (args[0].Equals("--mlc", StringComparison.OrdinalIgnoreCase))
         {
-            Console.WriteLine($@"
-            carlos_diaz | @dfirence
-            
-            SharpExperiments CLI
-            
-            Usage:
-                --repl      Start the interactive REPL mode.
-            ");
+            EDRBinaryClassifier.TrainAndRun();
         }
     }
 }
